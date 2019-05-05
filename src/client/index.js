b53de9158034af4ef9ae6673f881ceda7d4ae630
AlexKostiv <byrkotyn@gmail.com>
2019-05-05 20:31:49 +0300

    corrections

--- src/client/index.js
+++ src/client/index.js
@@ -13,12 +13,5 @@ WebFont.load({
   }
 });
 
-if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js').then((registration) => {
-    console.log('SW registered: ', registration);
-  }).catch((registrationError) => {
-    console.log('SW registration failed: ', registrationError);
-  });
-}
 
 ReactDOM.render(<WeatherApp />, document.getElementById('app'));
